a59cae658d19fa1695636b0f6c0e55cf36e88952
icycandle-blink <icycandle@blink.com.tw>
2019-01-28 09:47:46 +0800

    music detail view

--- src/components/BackGroundMusicDetail.vue
+++ src/components/BackGroundMusicDetail.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="music-detail">
-    <h5>{{workType}} BGM #{{bgmId}}</h5>
+    <h5>{{jobType}} BGM #{{bgmId}} By {{username}}</h5>
     <iframe
+      v-if="youtubeLink"
       width="560"
       height="315"
       :src="youtubeLink"
@@ -18,18 +19,50 @@
 
 export default {
   name: 'BackGroundMusicDetail',
+  methods: {
+    parseYouTubeHash(url) {
+      const pattern1 = /youtube\.com\/watch\?v=(wjVjHce8uME)/;
+      const pattern2 = /youtu\.be\/(wjVjHce8uME)/;
+      if (url.match(pattern1)) {
+        const regexResult = url.match(pattern1);
+        const hash = regexResult[1];
+        return hash;
+      } else if (url.match(pattern2)) {
+        const regexResult = url.match(pattern2);
+        const hash = regexResult[1];
+        return hash;
+      }
+      return false;
+    },
+  },
+  created() {
+    const vm = this;
+    const bgmId = this.$route.params.id;
+    const apiUrl = `/api/musiclinks/${bgmId}.json`;
+    this.axios({
+      url: apiUrl,
+      method: 'GET',
+    })
+      .then((result) => {
+        console.log(result);
+        vm.url = result.data.url;
+        vm.jobType = result.data.jobtype;
+        vm.comment = result.data.comment;
+        vm.username = result.data.username;
+        const youtubeHash = vm.parseYouTubeHash(vm.url);
+        vm.youtubeLink = `https://www.youtube.com/embed/${youtubeHash}`;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
   data() {
-    const bgmId = 13;
-    // const apiUrl = `/api/musiclinks/${bgmId}.json`;
-    const youtubeHash = 'YSMXbbw2B8Y';
-    const workType = '寫程式';
-    const youtubeLink = `https://www.youtube.com/embed/${youtubeHash}`;
-    const comment = 'lorem ipsem';
     return {
-      youtubeLink,
-      workType,
-      bgmId,
-      comment,
+      youtubeLink: '',
+      jobType: '',
+      bgmId: null,
+      comment: '',
+      username: '',
     };
   },
 };
