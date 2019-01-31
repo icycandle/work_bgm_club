bba975ab684b49e3d6231a506d6307e53651fc1f
icycandle-blink <icycandle@blink.com.tw>
2019-01-31 20:44:17 +0800

    admin.site.register
    Signed-off-by: icycandle-blink <icycandle@blink.com.tw>

--- musiclink/admin.py
+++ musiclink/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from musiclink.models import User
+from musiclink.models import User, UserQueue, LinkQueue, MusicLink, MusicRating
 
 admin.site.register(User, UserAdmin)
+
+class UserQueueAdmin(admin.ModelAdmin):
+    pass
+
+class LinkQueueAdmin(admin.ModelAdmin):
+    pass
+
+class MusicLinkAdmin(admin.ModelAdmin):
+    pass
+
+class MusicRatingAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserQueue, UserQueueAdmin)
+admin.site.register(LinkQueue, LinkQueueAdmin)
+admin.site.register(MusicLink, MusicLinkAdmin)
+admin.site.register(MusicRating, MusicRatingAdmin)
