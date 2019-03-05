30662f64897e8688ea7028238de8614dc9f19bd5
icycandle-blink <icycandle@blink.com.tw>
2019-03-05 21:04:00 +0800

    fix mail sending behavior and add admin action

--- musiclink/admin.py
+++ musiclink/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from musiclink.models import User, UserQueue, LinkQueue, MusicLink, MusicRating
 
-admin.site.register(User, UserAdmin)
+from musiclink.models import LinkQueue, MusicLink, MusicRating, User, UserQueue
+from musiclink.signals import admin_action_send_music_email_to_user
+
+
+def force_send_music_email(modeladmin, request, user_queryset):
+    for user in user_queryset:
+        admin_action_send_music_email_to_user.send(sender=User, user=user)
+force_send_music_email.short_description = "寄出音樂信件至此使用者的所有email"
+
+
+class MyUserAdmin(UserAdmin):
+    actions = [force_send_music_email]
+admin.site.register(User, MyUserAdmin)
 
 class UserQueueAdmin(admin.ModelAdmin):
     pass
--- musiclink/signals.py
+++ musiclink/signals.py
@@ -1,11 +1,41 @@
+from allauth.account.models import EmailAddress
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.dispatch import Signal, receiver
 
 from musiclink.mail import send_email
-from musiclink.models import LinkQueue, MusicLink, UserQueue
+from musiclink.models import LinkQueue, MusicLink, UserQueue, User
 
+admin_action_send_music_email_to_user = Signal(providing_args=["user"])
+
+@receiver(admin_action_send_music_email_to_user, sender=User)
+def admin_action_send_music_email_to_user_(sender, user, **kwargs):
+    seed_musiclink = user.music_links.last()
+    same_type_musiclink = MusicLink.objects.filter(
+        jobtype=seed_musiclink.jobtype,
+        pk__gt=seed_musiclink.pk,  # get newer
+    ).exclude(user=user)
+    if same_type_musiclink:
+        musiclink = same_type_musiclink.order_by('?')[0]
+        send_music_email_to_user(user, musiclink)
+
+def send_music_email_to_user(user, musiclink):
+    to_emails = EmailAddress.objects.filter(user=user).values_list('email', flat=True)
+    subject = '來自背景音樂俱樂部的「{0.jobtype}」音樂 #{0.id}'.format(musiclink)
+    site = 'https://' + get_current_site(None).domain
+    send_email(
+        subject=subject,
+        from_email='service@dwave.cc',
+        to_emails=list(to_emails),
+        template='mail_template.html',
+        context={
+            'user': user,
+            'subject': subject,
+            'musiclink': musiclink,
+            'site': site,
+        }
+    )
 
 @receiver(post_save, sender=MusicLink)
 def send_music_link_email(sender, **kwargs):
@@ -20,24 +50,6 @@ def send_music_link_email(sender, **kwargs):
     musiclink = kwargs['instance']
     jobtype = musiclink.jobtype
 
-    ############## test ##############
-    # to_email = 'icycandle@gmail.com'
-    # subject = '來自背景音樂俱樂部的「{0.jobtype}」音樂 #{0.id}'.format(musiclink)
-    # send_email(
-    #     subject=subject,
-    #     from_email='service@dwave.cc',
-    #     to_email=to_email,
-    #     template='mail_template.html',
-    #     context={
-    #         'user': user,
-    #         'subject': subject,
-    #         'musiclink': musiclink,
-    #         'site': 'https://workbgmclub.wearetags.com',
-    #     }
-    # )
-    # return
-    ############## test ##############
-
     user_from_queue = UserQueue.objects.filter(
         jobtype=jobtype,
     ).exclude(
@@ -45,21 +57,8 @@ def send_music_link_email(sender, **kwargs):
     ).order_by('create_at').first()
     if user_from_queue:
         # 代表有東西可寄了
-        to_email = user_from_queue.user.email
-        subject = '來自背景音樂俱樂部的「{0.jobtype}」音樂 #{0.id}'.format(musiclink)
-        site = 'https://' + get_current_site(None).domain
-        send_email(
-            subject=subject,
-            from_email='service@dwave.cc',
-            to_email=to_email,
-            template='mail_template.html',
-            context={
-                'user': user_from_queue.user,
-                'subject': subject,
-                'musiclink': musiclink,
-                'site': site,
-            }
-        )
+        user = user_from_queue.user
+        send_music_email_to_user(user, musiclink)
         user_from_queue.delete()
     else:
         #代表現在暫時沒有適合的使用者可寄，儲存至 LinkQueue
